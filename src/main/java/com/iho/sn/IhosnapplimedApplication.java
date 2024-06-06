92fb049985518965a3e11ab7964ef570374c1aeb
tairou <thirdiallo@gmail.com>
2024-06-06 22:48:17 +0000

    initial commit

---
+++ src/main/java/com/iho/sn/IhosnapplimedApplication.java
@@ -0,0 +1,13 @@
+package com.iho.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhosnapplimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhosnapplimedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iho/sn/IhosnapplimedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iho.sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhosnapplimedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
